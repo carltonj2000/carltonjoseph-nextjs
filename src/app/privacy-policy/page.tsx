da6a486ff2c22551f9a444bd3def94e7b477636c
Carlton Joseph <carlton.joseph@gmail.com>
2024-03-19 14:44:09 -0700

    initial commit

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,36 @@
+export default function PrivacyPolicy() {
+  return (
+    <>
+      <h1>Privacy Policy</h1>
+      <ul className="list-disc">
+        <li>
+          We respect your privacy. We won&amp;t use your E-mail id and name
+          other than sending you information about our applications and will
+          only contact you in reference to any issue or bugs you have filed
+          related to our applications.
+        </li>
+        <li>
+          We won&amp;t share your personal data e.g. E-mail id, Contact Number
+          with any third parties.
+        </li>
+        <li>
+          We may display some paid advertisement on the website, that may lead
+          you to some external website and we are not responsible for external
+          website.
+        </li>
+        <li>
+          We or any of our third-party advertisers, may add a cookie to
+          determine targeted advertisements based on your preferences. You can
+          choose to opt of cookies by visiting the third-party advertisers ad
+          and content network privacy policy. At present Google is the only
+          third-party advertiser.
+        </li>
+        <li>
+          If we send you any information regarding our application you can
+          opting out at anytime. We will stop e-mailing you as soon as you
+          opt-out.
+        </li>
+      </ul>
+    </>
+  );
+}
